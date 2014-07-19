3ba46cb9c9e3a83a2220a497d5840b9fa64400e8
Suguru YAMAMOTO <sgryjp@gmail.com>
2014-07-19 15:21:46 +0900

    Moved source files to a subdirectory.

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Sgry.Ini" )]
+[assembly: AssemblyDescription( "A single file INI file parser written in C#." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Sgry.Ini" )]
+[assembly: AssemblyCopyright( "Copyright © 2006-2009 Suguru Yamamoto" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+[assembly: Guid( "aae9505a-b773-4275-a5fa-029310bd64c3" )]
+
+[assembly: AssemblyVersion( "2.1.1.*" )]
+[assembly: AssemblyFileVersion( "2.1.1.0" )]
